73d71d6187d322b6abd3a732609557d330c1e507
Cube <hardijs@cubemobile.lv>
2022-12-31 14:13:41 +0200

    Add empty package

---
+++ DevTools/Sources/DevTools/RequestManager.swift
@@ -0,0 +1,14 @@
+//
+//  RequestManager.swift
+//  
+//
+//  Created by Cube on 31/12/2022.
+//
+
+import Moya
+
+class RequestManager {
+    func doSomething() {
+        print("Did something")
+    }
+}
