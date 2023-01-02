4a9b28c471986904923e9ce6baae11b8e09205b4
Cube <hardijs@cubemobile.lv>
2023-01-02 18:27:47 +0200

    Add iOS 13 platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "DevTools",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -17,7 +20,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Moya/Moya", .upToNextMajor(from: "15.0.0")),
 //        .package(url: "https://github.com/realm/realm-swift", .upToNextMajor(from: "10.0.0"))
-        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa", from: "10.7.0")
+        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa", .upToNextMajor(from: "10.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
