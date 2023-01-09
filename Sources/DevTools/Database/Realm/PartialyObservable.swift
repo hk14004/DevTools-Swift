e4e5a27c21ece80f1ecebf539585a5253f4ec380
hk14004 <hardijs.kirsis@gmail.com>
2023-01-09 14:07:46 +0200

    Add PartialyObservable

---
+++ Sources/DevTools/Database/Realm/PartialyObservable.swift
@@ -0,0 +1,28 @@
+//
+//  PartialyObservable.swift
+//  
+//
+//  Created by Hardijs Ķirsis on 09/01/2023.
+//
+
+import RealmSwift
+
+public protocol OservableField: RawRepresentable, CaseIterable, Hashable where RawValue == String {
+
+}
+
+public protocol PartialyObservable: AnyObject where Self: Object {
+    associatedtype FieldType: OservableField
+    associatedtype ObjectType: Object
+    func observe(fields: Set<FieldType>, closure:(ObjectChange<ObjectType>) -> Void) -> NotificationToken
+}
+
+
+extension PartialyObservable {
+    func observe(fields: Set<FieldType>, closure: @escaping (ObjectChange<ObjectType>) -> Void) -> NotificationToken {
+        let keyPaths = fields.map { field in
+            field.rawValue
+        }
+        return observe(keyPaths: keyPaths, closure)
+    }
+}
