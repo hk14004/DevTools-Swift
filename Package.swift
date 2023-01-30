1d77333c6e49c2364219d708ccc741538d7abc67
Cube <hardijs@cubemobile.lv>
2023-01-30 18:39:53 +0200

    Tweak package realm file

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Moya/Moya", .upToNextMajor(from: "15.0.0")),
-        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa", .upToNextMajor(from: "10.0.0"))
+        .package(url: "https://github.com/realm/realm-cocoa", .upToNextMajor(from: "10.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -52,6 +52,6 @@ let package = Package(
             dependencies: []),
         .target(
             name: "DevToolsRealm",
-            dependencies: [.product(name: "RealmSwift", package: "Realm")]),
+            dependencies: [.product(name: "RealmSwift", package: "realm-cocoa")]),
     ]
 )
