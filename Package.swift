b2c530a90eaf3b62372d53878ef2ef9b9e1c1159
Cube <hardijs@cubemobile.lv>
2023-01-30 18:18:19 +0200

    Split package into chunks

--- Package.swift
+++ Package.swift
@@ -13,13 +13,23 @@ let package = Package(
         .library(
             name: "DevTools",
             targets: ["DevTools"]),
-//        .product(name: "RealmSwift", package: "realm-swift")
+        .library(
+            name: "DevToolsNavigation",
+            targets: ["DevToolsNavigation"]),
+        .library(
+            name: "DevToolsNetworking",
+            targets: ["DevToolsNetworking"]),
+        .library(
+            name: "DevToolsUI",
+            targets: ["DevToolsUI"]),
+        .library(
+            name: "DevToolsRealm",
+            targets: ["DevToolsRealm"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Moya/Moya", .upToNextMajor(from: "15.0.0")),
-//        .package(url: "https://github.com/realm/realm-swift", .upToNextMajor(from: "10.0.0"))
         .package(name: "Realm", url: "https://github.com/realm/realm-cocoa", .upToNextMajor(from: "10.0.0"))
     ],
     targets: [
@@ -27,9 +37,21 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DevTools",
-            dependencies: ["Moya",.product(name: "RealmSwift", package: "Realm")]),
+            dependencies: []),
         .testTarget(
             name: "DevToolsTests",
             dependencies: ["DevTools"]),
+        .target(
+            name: "DevToolsNavigation",
+            dependencies: []),
+        .target(
+            name: "DevToolsNetworking",
+            dependencies: ["Moya"]),
+        .target(
+            name: "DevToolsUI",
+            dependencies: []),
+        .target(
+            name: "DevToolsRealm",
+            dependencies: [.product(name: "RealmSwift", package: "Realm")]),
     ]
 )
