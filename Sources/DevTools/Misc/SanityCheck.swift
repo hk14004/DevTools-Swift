6e46a476a9cf7f1423511eaeef5f214630c8482e
Cube <hardijs@cubemobile.lv>
2023-02-01 12:15:40 +0200

    Add sanity check print

---
+++ Sources/DevTools/Misc/SanityCheck.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Cube on 01/02/2023.
+//
+
+import Foundation
+
+func sanityCheck(name: String = "SANITY-CHECK", operation: ()->()) {
+    let symbols = "###########"
+    print(symbols + name + "-START" + symbols)
+    operation()
+    print(symbols + name + "-END" + symbols)
+}
