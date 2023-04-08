d31b2b96149ebd4df56a2ce006592579ce74d966
hk14004 <hardijs.kirsis@gmail.com>
2023-04-08 15:41:44 +0300

    Add DevToolsCore

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "DevTools",
-            targets: ["DevTools"]),
+            name: "DevToolsCore",
+            targets: ["DevToolsCore"]),
         .library(
             name: "DevToolsNavigation",
             targets: ["DevToolsNavigation"]),
@@ -39,11 +39,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "DevTools",
+            name: "DevToolsCore",
             dependencies: []),
         .testTarget(
             name: "DevToolsTests",
-            dependencies: ["DevTools"]),
+            dependencies: ["DevToolsCore"]),
         .target(
             name: "DevToolsNavigation",
             dependencies: []),
@@ -55,9 +55,9 @@ let package = Package(
             dependencies: []),
         .target(
             name: "DevToolsRealm",
-            dependencies: [.product(name: "RealmSwift", package: "realm-cocoa"), "DevTools"]),
+            dependencies: [.product(name: "RealmSwift", package: "realm-cocoa"), "DevToolsCore"]),
         .target(
             name: "DevToolsCoreData",
-            dependencies: ["DevTools"]),
+            dependencies: ["DevToolsCore"]),
     ]
 )
