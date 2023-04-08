110bf915b2e2a303d62b71d4ddb0030b8456995f
hk14004 <hardijs.kirsis@gmail.com>
2023-04-08 19:25:19 +0300

    Add paging group

--- Sources/DevToolsCore/Clean/PersistentStore/PersistedLayerInterface.swift
+++ Sources/DevToolsCore/Clean/PersistentStore/PersistedLayerInterface.swift
@@ -8,6 +8,29 @@
 import Foundation
 import Combine
 
+public protocol PersistedLayerInterface {
+    associatedtype T: PersistableDomainModelProtocol
+    
+    // Read & Observe
+    @discardableResult func getSingle(id: String) async -> T?
+    @discardableResult func getList(predicate: NSPredicate,
+                                    sortedByKeyPath: String,
+                                    ascending: Bool) async -> [T]
+    @discardableResult func getListPage(pageOptions: PagedRequestOptions, predicate: NSPredicate,
+                                    sortedByKeyPath: String,
+                                    ascending: Bool) async -> PagedResult<T>
+    @discardableResult func observeSingle(id: String) -> AnyPublisher<T?,Never>
+    @discardableResult func observeList(predicate: NSPredicate,
+                                        sortedByKeyPath: String,
+                                        ascending: Bool) -> AnyPublisher<[T],Never>
+    
+    // Write
+    func addOrUpdate(_ items: [T]) async
+    func delete(_ itemIds: [String]) async
+    func replace(with items: [T]) async
+    func bulkWrite(operations: [() async -> Void]) async
+}
+
 open class BasePersistedLayerInterface<T: PersistableDomainModelProtocol>: PersistedLayerInterface {
     
     public init() {}
@@ -46,50 +69,5 @@ open class BasePersistedLayerInterface<T: PersistableDomainModelProtocol>: Persi
     
     open func bulkWrite(operations: [() async -> Void]) async {
         fatalError()
-    }
-}
-
-public protocol PersistedLayerInterface {
-    associatedtype T: PersistableDomainModelProtocol
-    
-    // Read & Observe
-    @discardableResult func getSingle(id: String) async -> T?
-    @discardableResult func getList(predicate: NSPredicate,
-                                    sortedByKeyPath: String,
-                                    ascending: Bool) async -> [T]
-    @discardableResult func getListPage(pageOptions: PagedRequestOptions, predicate: NSPredicate,
-                                    sortedByKeyPath: String,
-                                    ascending: Bool) async -> PagedResult<T>
-    @discardableResult func observeSingle(id: String) -> AnyPublisher<T?,Never>
-    @discardableResult func observeList(predicate: NSPredicate,
-                                        sortedByKeyPath: String,
-                                        ascending: Bool) -> AnyPublisher<[T],Never>
-    
-    // Write
-    func addOrUpdate(_ items: [T]) async
-    func delete(_ itemIds: [String]) async
-    func replace(with items: [T]) async
-    func bulkWrite(operations: [() async -> Void]) async
-}
-
-public struct PagedResult<T> {
-    public let pageNumber: Int
-    public let pageItems: [T]
-    public let hasNextPage: Bool
-    
-    public init(pageNumber: Int, pageItems: [T], hasNextPage: Bool) {
-        self.pageNumber = pageNumber
-        self.pageItems = pageItems
-        self.hasNextPage = hasNextPage
-    }
-    
-}
-public struct PagedRequestOptions {
-    public let fetchPage: Int
-    public let pageSize: Int
-    
-    public init(fetchPage: Int, pageSize: Int) {
-        self.fetchPage = fetchPage
-        self.pageSize = pageSize
     }
 }
