f8b40e7da84421886c114d1edaf0de3675d269fa
hk14004 <hardijs.kirsis@gmail.com>
2023-05-05 21:02:42 +0300

    Tweak UISectionModelProtocol

--- Sources/DevToolsUI/Section/UISectionModelProtocol.swift
+++ Sources/DevToolsUI/Section/UISectionModelProtocol.swift
@@ -11,7 +11,7 @@ public protocol UISectionModelProtocol {
     associatedtype Cell: Hashable
     associatedtype Identifier: CaseIterable, RawRepresentable, Hashable where Identifier.RawValue == String
     
-    var uuid: String { get }
+    var identifier: Identifier { get }
     var title: String { get set }
     var cells: [Cell] { get set}
 }
@@ -28,12 +28,12 @@ fileprivate struct ExampleSection: UISectionModelProtocol {
         case showText
     }
     
-    let uuid: String
+    let identifier: Identifier
     var title: String
     var cells: [Cell]
     
-    init(uuid: String, title: String, cells: [Cell]) {
-        self.uuid = uuid
+    init(identifier: Identifier, title: String, cells: [Cell]) {
+        self.identifier = identifier
         self.title = title
         self.cells = cells
     }
@@ -41,25 +41,25 @@ fileprivate struct ExampleSection: UISectionModelProtocol {
 
 public extension Array where Element: UISectionModelProtocol {
     mutating func update(section: Element) {
-        guard let sectionIndex = firstIndex(where: {$0.uuid == section.uuid}) else {
+        guard let sectionIndex = firstIndex(where: {$0.identifier == section.identifier}) else {
             return
         }
         remove(at: sectionIndex)
         insert(section, at: sectionIndex)
     }
     
-    func getSection(uuid: String) -> Element? {
-        return first(where: {$0.uuid == uuid})
+    func getSection(id: Element.Identifier) -> Element? {
+        return first(where: {$0.identifier == id})
     }
     
     mutating func remove(section: Element) {
-        guard let sectionIndex = firstIndex(where: {$0.uuid == section.uuid}) else {
+        guard let sectionIndex = firstIndex(where: {$0.identifier == section.identifier}) else {
             return
         }
         remove(at: sectionIndex)
     }
     
-    func hasSection(uuid: String) -> Bool {
-        return contains(where: {$0.uuid == uuid})
+    func hasSection(id: Element.Identifier) -> Bool {
+        return contains(where: {$0.identifier == id})
     }
 }
