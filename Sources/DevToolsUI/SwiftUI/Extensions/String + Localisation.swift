24c6a3c74b784372e6f492699dd2c318803661a3
hk14004 <hardijs.kirsis@gmail.com>
2023-05-27 20:21:15 +0300

    Add string to swiftui localised

---
+++ Sources/DevToolsUI/SwiftUI/Extensions/String + Localisation.swift
@@ -0,0 +1,14 @@
+//
+//  String + Localisation.swift
+//  
+//
+//  Created by Hardijs Ķirsis on 27/05/2023.
+//
+
+import SwiftUI
+
+extension String {
+    func toSwiftUILocalised() -> LocalizedStringKey {
+        .init(self)
+    }
+}
