ade99c0d657143a73114373afaf3028a5d6e54f6
hk14004 <hardijs.kirsis@gmail.com>
2023-05-28 10:33:38 +0300

    Add callback types

---
+++ Sources/DevToolsCore/Foundation/Typealias.swift
@@ -0,0 +1,11 @@
+//
+//  Typealias.swift
+//  
+//
+//  Created by Hardijs Ķirsis on 28/05/2023.
+//
+
+import Foundation
+
+public typealias VoidCallback = (() -> ())
+public typealias BoolCallback = ((Bool) -> ())
