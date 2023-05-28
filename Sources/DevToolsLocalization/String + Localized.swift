c4e670b425e461513e22634387ead2aeb671787a
hk14004 <hardijs.kirsis@gmail.com>
2023-05-28 21:19:31 +0300

    Updates

---
+++ Sources/DevToolsLocalization/String + Localized.swift
@@ -0,0 +1,15 @@
+//
+//  String + Localized.swift
+//  
+//
+//  Created by Hardijs Ķirsis on 28/05/2023.
+//
+
+import Foundation
+import SwiftUI
+
+public extension String {
+    func localizedRuntimeString() -> String {
+        self.localized()
+    }
+}
