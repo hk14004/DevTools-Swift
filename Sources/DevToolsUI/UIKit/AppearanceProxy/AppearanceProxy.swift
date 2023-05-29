3b0578e95584d7817c3813383c561d0ec9ea8abd
hk14004 <hardijs.kirsis@gmail.com>
2023-05-30 01:40:24 +0300

    Update

--- Sources/DevToolsUI/UIKit/AppearanceProxy/AppearanceProxy.swift
+++ Sources/DevToolsUI/UIKit/AppearanceProxy/AppearanceProxy.swift
@@ -17,55 +17,55 @@ public extension AppearanceProxy {
     
     // MARK: Content
     
-    func setDefault(contentTextColor: UIColor) {
+    static func setDefault(contentTextColor: UIColor) {
         UILabel.appearance().textColor = contentTextColor
         UITextField.appearance().textColor = contentTextColor
         UITextView.appearance().textColor = contentTextColor
         UIButton.appearance().setTitleColor(contentTextColor, for: .normal)
     }
     
-    func setDefault(contentFont: UIFont) {
+    static func setDefault(contentFont: UIFont) {
         UILabel.appearance().font = contentFont
         UITextField.appearance().font = contentFont
         UITextView.appearance().font = contentFont
     }
     
-    func setDefault(contentBackgroundColor: UIColor) {
+    static func setDefault(contentBackgroundColor: UIColor) {
         UIView.appearance().backgroundColor = contentBackgroundColor
     }
     
-    func setDefault(controlTintColor: UIColor) {
+    static func setDefault(controlTintColor: UIColor) {
         UIControl.appearance().tintColor = controlTintColor
     }
     
     // MARK: Navigation bar
     
-    func setDefault(navigationBarBackgroundColor: UIColor) {
+    static func setDefault(navigationBarBackgroundColor: UIColor) {
         UINavigationBar.appearance().barTintColor = navigationBarBackgroundColor
     }
     
-    func setDefault(navigationBarTitleTextAttributes: [NSAttributedString.Key: Any]) {
+    static func setDefault(navigationBarTitleTextAttributes: [NSAttributedString.Key: Any]) {
         UINavigationBar.appearance().titleTextAttributes = navigationBarTitleTextAttributes
     }
     
-    func setDefault(navigationBarTranslucent: Bool) {
+    static func setDefault(navigationBarTranslucent: Bool) {
         UINavigationBar.appearance().isTranslucent = navigationBarTranslucent
     }
     
-    func setDefault(navigationBarShadowImage: UIImage?) {
+    static func setDefault(navigationBarShadowImage: UIImage?) {
         UINavigationBar.appearance().shadowImage = navigationBarShadowImage
     }
     
-    func setDefault(navigationBarBackgroundImage: UIImage?) {
+    static func setDefault(navigationBarBackgroundImage: UIImage?) {
         UINavigationBar.appearance().setBackgroundImage(navigationBarBackgroundImage, for: .default)
     }
     
-    func setDefault(navigationBarBackIndicatorImage: UIImage?) {
+    static func setDefault(navigationBarBackIndicatorImage: UIImage?) {
         UINavigationBar.appearance().backIndicatorImage = navigationBarBackIndicatorImage
         UINavigationBar.appearance().backIndicatorTransitionMaskImage = navigationBarBackIndicatorImage
     }
     
-    func setDefault(navigationBarTintColor: UIColor) {
+    static func setDefault(navigationBarTintColor: UIColor) {
         UINavigationBar.appearance().tintColor = navigationBarTintColor
     }
     
