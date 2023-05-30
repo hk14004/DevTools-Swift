d080879cf0f9df7b4a2f3330d243523a78438e11
hk14004 <hardijs.kirsis@gmail.com>
2023-05-30 21:20:56 +0300

    Tweak switch

--- Sources/DevToolsUI/UIKit/AppearanceProxy/AppearanceProxy.swift
+++ Sources/DevToolsUI/UIKit/AppearanceProxy/AppearanceProxy.swift
@@ -36,7 +36,7 @@ public extension AppearanceProxy {
     
     static func setDefault(controlTintColor: UIColor) {
         UIControl.appearance().tintColor = controlTintColor
-        UISwitch.appearance().tintColor = controlTintColor
+        UISwitch.appearance().thumbTintColor = controlTintColor
     }
     
     // MARK: Navigation bar
