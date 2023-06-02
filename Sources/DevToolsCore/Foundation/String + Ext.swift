f8aa877c1dd45da281d042203e67e0170a378516
hk14004 <hardijs.kirsis@gmail.com>
2023-06-02 16:01:26 +0300

    Add htmlToAttributedString

--- Sources/DevToolsCore/Foundation/String + Ext.swift
+++ Sources/DevToolsCore/Foundation/String + Ext.swift
@@ -13,4 +13,23 @@ extension String {
         let regex = try! NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}", options: .caseInsensitive)
         return regex.firstMatch(in: self, options: [], range: NSRange(location: 0, length: count)) != nil
     }
+    
+    func htmlToAttributedString() -> NSAttributedString? {
+        guard let data = data(using: .utf8) else {
+            return nil
+        }
+        
+        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+            .documentType: NSAttributedString.DocumentType.html,
+            .characterEncoding: String.Encoding.utf8.rawValue
+        ]
+        
+        do {
+            let attributedString = try NSAttributedString(data: data, options: options, documentAttributes: nil)
+            return attributedString
+        } catch {
+            print("Error converting HTML to attributed string: \(error)")
+            return nil
+        }
+    }
 }
