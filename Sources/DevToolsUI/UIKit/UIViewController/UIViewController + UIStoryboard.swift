921088347d475fa83295e12ecc78eb98851f2b32
hk14004 <hardijs.kirsis@gmail.com>
2023-06-11 14:54:00 +0300

    Fix api

--- Sources/DevToolsUI/UIKit/UIViewController/UIViewController + UIStoryboard.swift
+++ Sources/DevToolsUI/UIKit/UIViewController/UIViewController + UIStoryboard.swift
@@ -8,11 +8,35 @@
 import UIKit
 
 public extension UIViewController {
-    static func instantiateViewController(creator: ((NSCoder) -> Self)?) -> Self! {
-        return UIStoryboard.instantiateViewController(type: Self.self, creator: creator)!
+    static func instantiateViewController<T: UIViewController>(creator: ((NSCoder) -> T)?) -> T! {
+        return UIStoryboard.instantiateViewController(type: T.self, creator: creator)!
     }
     
     static func instantiateViewController() -> Self! {
         return UIStoryboard.instantiateViewController(type: Self.self)!
     }
 }
+
+fileprivate class ExampleVC: UIViewController {
+    
+    let injeted: Int
+    
+    required init?(coder: NSCoder) {
+        injeted = 0
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder, injected: Int) {
+        self.injeted = injected
+        super.init(coder: coder)
+    }
+}
+
+fileprivate func example() {
+    let vc = ExampleVC.instantiateViewController { coder in
+        ExampleVC(coder: coder, injected: 999)!
+    }!
+    print(vc)
+}
+
+
