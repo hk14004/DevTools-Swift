92c4ea39d74964676acf4e91f82ce1a580f72b2b
hk14004 <hardijs.kirsis@gmail.com>
2023-06-12 21:17:10 +0300

    Add UIViewController ext

---
+++ Sources/DevToolsUI/UIKit/UIViewController/UIViewController + Ext.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController + Ext.swift
+//  
+//
+//  Created by Hardijs Ķirsis on 12/06/2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    static func getTopViewController() -> UIViewController? {
+        guard let keyWindow = UIApplication.shared.connectedScenes
+            .compactMap({ $0 as? UIWindowScene })
+            .first(where: { $0.activationState == .foregroundActive })?.windows
+            .first(where: { $0.isKeyWindow })
+            else {
+                return nil
+            }
+        var top = keyWindow.rootViewController
+        while let next = top?.presentedViewController {
+            top = next
+        }
+        return top
+    }
+}
