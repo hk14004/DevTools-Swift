beb21f99f14b48e02c83f292a7262a2b6caa7416
hk14004 <hardijs.kirsis@gmail.com>
2023-06-12 16:30:09 +0300

    Add StoryboardLoadableView

---
+++ Sources/DevToolsUI/UIKit/Storyboard/StoryboardLoadableView.swift
@@ -0,0 +1,27 @@
+//
+//  StoryboardLoadableView.swift
+//  
+//
+//  Created by Hardijs Ķirsis on 12/06/2023.
+//
+
+import UIKit
+
+public protocol StoryboardLoadableView: UIView {
+    var loadedXibView: UIView! { get set }
+    func commonXibInit()
+}
+
+public extension StoryboardLoadableView {
+    func commonXibInit() {
+        let name = String(describing: type(of: self))
+        loadedXibView = Bundle.main.loadNibNamed(name, owner: self)!.first as? UIView
+        addSubview(loadedXibView)
+        
+        loadedXibView.translatesAutoresizingMaskIntoConstraints = false
+        loadedXibView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        loadedXibView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        loadedXibView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        loadedXibView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+}
---
+++ Sources/DevToolsUI/UIKit/Storyboard/UIStoryboard + Ext.swift
@@ -0,0 +1,22 @@
+//
+//  UIStoryBoard + Ext.swift
+//  
+//
+//  Created by Hardijs on 31/12/2022.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    public static func instantiateViewController<T: UIViewController>(type: T.Type, creator: ((NSCoder) -> T)?) -> T! {
+        let id = "\(T.self)"
+        let storyboard = UIStoryboard(name: id, bundle: nil)
+        return storyboard.instantiateViewController(identifier: id, creator: creator)
+    }
+    
+    public static func instantiateViewController<T: UIViewController>(type: T.Type) -> T! {
+        let id = "\(T.self)"
+        let storyboard = UIStoryboard(name: id, bundle: nil)
+        return (storyboard.instantiateViewController(withIdentifier: id) as! T)
+    }
+}
