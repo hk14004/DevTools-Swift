84a1177f2b38b200eab83adef7c432194b0b1e28
hk14004 <hardijs.kirsis@gmail.com>
2023-06-13 21:58:55 +0300

    Updates

---
+++ Sources/DevToolsUI/UIKit/UIView/UIView + Ext.swift
@@ -0,0 +1,35 @@
+//
+//  UIView + Ext.swift
+//  
+//
+//  Created by Hardijs Ķirsis on 13/06/2023.
+//
+
+import UIKit
+
+public extension UIView {
+    func parentView<T: UIView>(of type: T.Type) -> T? {
+        guard let view = superview else {
+            return nil
+        }
+        return (view as? T) ?? view.parentView(of: T.self)
+    }
+    
+    @available(iOS 13.4, *)
+    func setMargins(direction: UIAxis, constant: CGFloat) {
+        switch direction {
+        case .both:
+            directionalLayoutMargins = .init(top: constant, leading: constant,
+                                             bottom: constant, trailing: constant)
+        case .horizontal:
+            directionalLayoutMargins.leading = constant
+            directionalLayoutMargins.trailing = constant
+        case .vertical:
+            directionalLayoutMargins.top = constant
+            directionalLayoutMargins.bottom = constant
+        default:
+            directionalLayoutMargins = .init(top: constant, leading: constant,
+                                             bottom: constant, trailing: constant)
+        }
+    }
+}
