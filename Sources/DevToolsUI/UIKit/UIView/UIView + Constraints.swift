a8fc5eeb3462e838832b84e9d376a1f7b57ec8dc
hk14004 <hardijs.kirsis@gmail.com>
2023-06-22 21:57:10 +0300

    Update pin to superview

--- Sources/DevToolsUI/UIKit/UIView/UIView + Constraints.swift
+++ Sources/DevToolsUI/UIKit/UIView/UIView + Constraints.swift
@@ -8,28 +8,34 @@
 import UIKit
 
 public extension UIView {
-    func pinToSuperviewEdges(useSafeArea: Bool) {
-        guard let superview = self.superview else {
+    func pinToSuperviewEdges(pinToSafeArea: Bool) {
+        guard let superview = superview else {
             return
         }
         
-        self.translatesAutoresizingMaskIntoConstraints = false
+        translatesAutoresizingMaskIntoConstraints = false
         
         let topAnchor: NSLayoutYAxisAnchor
+        let leadingAnchor: NSLayoutXAxisAnchor
+        let trailingAnchor: NSLayoutXAxisAnchor
         let bottomAnchor: NSLayoutYAxisAnchor
         
-        if useSafeArea {
+        if pinToSafeArea {
             topAnchor = superview.safeAreaLayoutGuide.topAnchor
+            leadingAnchor = superview.safeAreaLayoutGuide.leadingAnchor
+            trailingAnchor = superview.safeAreaLayoutGuide.trailingAnchor
             bottomAnchor = superview.safeAreaLayoutGuide.bottomAnchor
         } else {
             topAnchor = superview.topAnchor
+            leadingAnchor = superview.leadingAnchor
+            trailingAnchor = superview.trailingAnchor
             bottomAnchor = superview.bottomAnchor
         }
         
         NSLayoutConstraint.activate([
             self.topAnchor.constraint(equalTo: topAnchor),
-            self.leadingAnchor.constraint(equalTo: superview.leadingAnchor),
-            self.trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            self.leadingAnchor.constraint(equalTo: leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: trailingAnchor),
             self.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
