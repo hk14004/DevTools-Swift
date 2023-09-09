da64c9ee2662df9fe4d8c4fe86eab97812512823
hk14004 <hardijs.kirsis@gmail.com>
2023-09-09 15:47:15 +0300

    Theme

--- Sources/DevToolsUI/UIKit/Theme/Theme.swift
+++ Sources/DevToolsUI/UIKit/Theme/Theme.swift
@@ -9,18 +9,18 @@ import UIKit
 
 public protocol DevTheme {
     // MARK: - Misc
-    var roundedCornerRadius: CGFloat { get set }
+    static var roundedCornerRadius: CGFloat { get set }
     
     // MARK: - Backgrounds
-    var contentColor: UIColor { get set }
-    var backgroundColor: UIColor { get set }
+    static var contentColor: UIColor { get set }
+    static var backgroundColor: UIColor { get set }
     
     // MARK: - Buttons
-    var uIControlColor: UIColor { get set }
+    static var uIControlColor: UIColor { get set }
     
     // MARK: - Texts
-    var textColor: UIColor { get set }
-    var secondaryTextColor: UIColor { get set }
-    var placeHolderTextColor: UIColor { get set }
-    var textLinkColor: UIColor { get set }
+    static var textColor: UIColor { get set }
+    static var secondaryTextColor: UIColor { get set }
+    static var placeHolderTextColor: UIColor { get set }
+    static var textLinkColor: UIColor { get set }
 }
