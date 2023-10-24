5532b5df6b60e92c57fac68c6827d3473ef3d4a4
hk14004 <hardijs.kirsis@gmail.com>
2023-10-24 17:20:47 +0300

    Tweak await api

--- Sources/DevToolsCore/Frameworks+Ext/XCTestCase/XCTestCase+Combine.swift
+++ Sources/DevToolsCore/Frameworks+Ext/XCTestCase/XCTestCase+Combine.swift
@@ -14,11 +14,9 @@ public extension XCTestCase {
         timeout: TimeInterval = 10,
         file: StaticString = #file,
         line: UInt = #line
-    ) throws -> T.Output {
-        // This time, we use Swift's Result type to keep track
-        // of the result of our Combine pipeline:
+    ) throws -> Result<T.Output, Error>? {
         var result: Result<T.Output, Error>?
-        let expectation = expectation(description: "Awaiting publisher")
+        let expectation = expectation(description: "Awaiting publisher file: \(file), line:\(line)")
 
         let cancellable = publisher.sink(
             receiveCompletion: { completion in
@@ -36,24 +34,10 @@ public extension XCTestCase {
             }
         )
 
-        // Just like before, we await the expectation that we
-        // created at the top of our test, and once done, we
-        // also cancel our cancellable to avoid getting any
-        // unused variable warnings:
         waitForExpectations(timeout: timeout)
         cancellable.cancel()
 
-        // Here we pass the original file and line number that
-        // our utility was called at, to tell XCTest to report
-        // any encountered errors at that original call site:
-        let unwrappedResult = try XCTUnwrap(
-            result,
-            "Awaited publisher did not produce any output",
-            file: file,
-            line: line
-        )
-
-        return try unwrappedResult.get()
+        return result
     }
 }
 
