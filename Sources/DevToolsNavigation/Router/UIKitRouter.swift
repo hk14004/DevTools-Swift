0aaf120744a9084292da382c6bdd87df092b9344
hk14004 <hardijs.kirsis@gmail.com>
2023-10-25 21:32:48 +0300

    Add router navigation controller getter

--- Sources/DevToolsNavigation/Router/UIKitRouter.swift
+++ Sources/DevToolsNavigation/Router/UIKitRouter.swift
@@ -10,3 +10,13 @@ import UIKit
 public protocol UIKitRouter {
     var viewController: UIViewController { get set }
 }
+
+public extension UIKitRouter {
+    var navigationController: UINavigationController? {
+        if let navVC = viewController as? UINavigationController {
+            return navVC
+        } else {
+            return viewController.navigationController
+        }
+    }
+}
