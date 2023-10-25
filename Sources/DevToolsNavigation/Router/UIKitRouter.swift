ec8c93f5acca86fb182d1dc6b5687c7d34cd483d
hk14004 <hardijs.kirsis@gmail.com>
2023-10-25 21:28:15 +0300

    Add uikit router

---
+++ Sources/DevToolsNavigation/Router/UIKitRouter.swift
@@ -0,0 +1,12 @@
+//
+//  UIKitRouter.swift
+//
+//
+//  Created by Hardijs Ķirsis on 25/10/2023.
+//
+
+import UIKit
+
+public protocol UIKitRouter {
+    var viewController: UIViewController { get set }
+}
