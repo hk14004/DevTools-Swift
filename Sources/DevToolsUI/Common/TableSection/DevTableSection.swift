8e4d1d394d51b6257f4c3ec68e3c189ea0e97d00
hk14004 <hardijs.kirsis@gmail.com>
2023-12-24 01:30:38 +0200

    Add default hasher

--- Sources/DevToolsUI/Common/TableSection/DevTableSection.swift
+++ Sources/DevToolsUI/Common/TableSection/DevTableSection.swift
@@ -9,10 +9,29 @@ import Foundation
 import DevToolsCore
 
 public protocol DevTableSection {
-    associatedtype Cell: Hashable
+    associatedtype Cell: Hashable, ContentComparable
     associatedtype Identifier: CaseIterable, RawRepresentable, Hashable where Identifier.RawValue == String
     
     var identifier: Identifier { get }
     var title: String { get set }
     var cells: [Cell] { get set}
+}
+
+public extension ContentComparable where Self: Hashable {
+    var contentHash: Int {
+        var hasher = Hasher()
+        let mirror = Mirror(reflecting: self)
+
+        for case let (_?, value) in mirror.children {
+            if let value = value as? ContentComparable {
+                hasher.combine(value.contentHash)
+                continue
+            }
+            if let hashable = value as? (any Hashable) {
+                hasher.combine(hashable)
+            }
+        }
+
+        return hasher.finalize()
+    }
 }
