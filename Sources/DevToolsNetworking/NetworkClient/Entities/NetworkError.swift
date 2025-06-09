30ca8eeaf0a376e16ecd99b97d9803877beb9fc7
Hardijs Kirsis <hardijs.kirsis@gmail.com>
2025-06-09 18:53:04 +0300

    Add NetworkError description

--- Sources/DevToolsNetworking/NetworkClient/Entities/NetworkError.swift
+++ Sources/DevToolsNetworking/NetworkClient/Entities/NetworkError.swift
@@ -9,3 +9,24 @@ public enum NetworkError: Error, Equatable {
     case unexpectedResponse
     case resourceNotFound
 }
+
+extension NetworkError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .apiErrorResponse(let apiError):
+            return apiError.message
+        case .reachability:
+            return "No internet connection."
+        case .unauthorized:
+            return "Unauthorized access."
+        case .unexpected(let message):
+            return "Unexpected error: \(message)"
+        case .forbidden:
+            return "Forbidden request."
+        case .unexpectedResponse:
+            return "Unexpected server response."
+        case .resourceNotFound:
+            return "Resource not found."
+        }
+    }
+}
