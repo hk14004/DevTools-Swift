0f19a5ce3a90c293902790294a655df0b8f93101
Hardijs Kirsis <hardijs.kirsis@gmail.com>
2025-07-08 18:19:04 +0300

    Extend sink

--- Sources/DevToolsCore/Extensions/Combine/Publisher+Sink.swift
+++ Sources/DevToolsCore/Extensions/Combine/Publisher+Sink.swift
@@ -2,10 +2,27 @@ import Combine
 
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 extension Publisher {
-    public func sink() -> AnyCancellable {
-        sink(
-            receiveCompletion: { _ in },
-            receiveValue: { _ in }
+    public func sink(
+        optional receiveCompletion: @escaping (Subscribers.Completion<Self.Failure>) -> Void = { _ in },
+        optional receiveValue: @escaping (Self.Output) -> Void = { _ in }
+    ) -> AnyCancellable {
+        return sink(
+            receiveCompletion: receiveCompletion,
+            receiveValue: receiveValue
+        )
+    }
+    
+    public func sink(
+        receiveValue: @escaping (Self.Output) -> Void,
+        completionError: @escaping (Self.Failure) -> Void
+    ) -> AnyCancellable {
+        return self.sink(
+            receiveCompletion: { completion in
+                if case .failure(let error) = completion {
+                    completionError(error)
+                }
+            },
+            receiveValue: receiveValue
         )
     }
 }
