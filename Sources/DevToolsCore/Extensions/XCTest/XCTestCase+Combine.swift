46c9fe283ddce7f4255402bcd9e2012e477addd2
Hardijs <hardijs.kirsis@gmail.com>
2025-11-25 19:04:42 +0200

    Improve combine await publisher

--- Sources/DevToolsCore/Extensions/XCTest/XCTestCase+Combine.swift
+++ Sources/DevToolsCore/Extensions/XCTest/XCTestCase+Combine.swift
@@ -43,10 +43,10 @@ public extension XCTestCase {
         timeout: TimeInterval = 10,
         file: StaticString = #file,
         line: UInt = #line
-    ) throws -> Result<T.Output, Error>? {
+    ) throws -> T.Output {
         var result: Result<T.Output, Error>?
-        let expectation = expectation(description: "Awaiting publisher file: \(file), line:\(line)")
-
+        let expectation = self.expectation(description: "Awaiting publisher")
+        
         let cancellable = publisher.sink(
             receiveCompletion: { completion in
                 switch completion {
@@ -55,18 +55,25 @@ public extension XCTestCase {
                 case .finished:
                     break
                 }
-
+                
                 expectation.fulfill()
             },
             receiveValue: { value in
                 result = .success(value)
             }
         )
-
+        
         waitForExpectations(timeout: timeout)
         cancellable.cancel()
-
-        return result
+        
+        let unwrappedResult = try XCTUnwrap(
+            result,
+            "Awaited publisher did not produce any output",
+            file: file,
+            line: line
+        )
+        
+        return try unwrappedResult.get()
     }
 }
 
