7b0eb790de61316534e1cbe050746f594ae308bf
Hardijs <hardijs.kirsis@gmail.com>
2025-11-26 16:04:38 +0200

    Revert to old publisher method

--- Sources/DevToolsXCTest/Extensions/XCTest/XCTestCase+Combine.swift
+++ Sources/DevToolsXCTest/Extensions/XCTest/XCTestCase+Combine.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Hardijs Ķirsis on 24/10/2023.
 //
+
 #if canImport(XCTest)
 import XCTest
 import Combine
@@ -43,10 +44,10 @@ public extension XCTestCase {
         timeout: TimeInterval = 10,
         file: StaticString = #file,
         line: UInt = #line
-    ) throws -> T.Output {
+    ) throws -> Result<T.Output, Error>? {
         var result: Result<T.Output, Error>?
-        let expectation = self.expectation(description: "Awaiting publisher")
-        
+        let expectation = expectation(description: "Awaiting publisher file: \(file), line:\(line)")
+
         let cancellable = publisher.sink(
             receiveCompletion: { completion in
                 switch completion {
@@ -55,25 +56,18 @@ public extension XCTestCase {
                 case .finished:
                     break
                 }
-                
+
                 expectation.fulfill()
             },
             receiveValue: { value in
                 result = .success(value)
             }
         )
-        
+
         waitForExpectations(timeout: timeout)
         cancellable.cancel()
-        
-        let unwrappedResult = try XCTUnwrap(
-            result,
-            "Awaited publisher did not produce any output",
-            file: file,
-            line: line
-        )
-        
-        return try unwrappedResult.get()
+
+        return result
     }
 }
 
